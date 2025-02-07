1b1c890ce9f26449882e0e0ce59d0d78e18527cd
JC Botelho <joaoclaudiobr92@gmail.com>
2025-02-07 13:53:36 -0300

    ajuste da home e inclusao do sorteador

--- publish.sh
+++ publish.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-
-
-# Compila o Sass
-#sass css/scss/styles.scss css/styles.css
-
-# Publica no S3
 aws s3 cp ./ s3://jcbotelho.com --recursive --acl public-read --exclude ".git/*" --exclude ".gitignore" --exclude "publish.sh" --exclude "README.md" --exclude "docs/*" --exclude "replaces/*" --exclude "clone.sh" --exclude "replaces.sh" --exclude "run.sh" --exclude "css/scss/*" --exclude "exemple/*"
-
-# Invalida o cache do CloudFront
 aws cloudfront create-invalidation --distribution-id E1XSEZ5D2XM7HJ --paths "/*"
