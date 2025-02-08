67558b2b9c85352d4cc64d569dde742ae52b5e54
JC Botelho <joaoclaudiobr92@gmail.com>
2025-02-08 20:55:31 -0300

    melhorias no sorteio: mutar som e gif randomico

--- publish.sh
+++ publish.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+
+
+# Compila o Sass
+#sass css/scss/styles.scss css/styles.css
+
+# Publica no S3
 aws s3 cp ./ s3://jcbotelho.com --recursive --acl public-read --exclude ".git/*" --exclude ".gitignore" --exclude "publish.sh" --exclude "README.md" --exclude "docs/*" --exclude "replaces/*" --exclude "clone.sh" --exclude "replaces.sh" --exclude "run.sh" --exclude "css/scss/*" --exclude "exemple/*"
-aws cloudfront create-invalidation --distribution-id E1XSEZ5D2XM7HJ --paths "/*"
+
+# Invalida o cache do CloudFront
+aws cloudfront create-invalidation --distribution-id EFTEVO3P436H1 --paths "/*"
